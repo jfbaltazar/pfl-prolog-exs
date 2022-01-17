a4196dcd0391e2bb9f855ff352a14e1c0c5a21f6
Joao Baltazar <joaoferreirabaltazar@gmail.com>
2022-01-17 23:29:58 +0000

    Add 2. a)-c)

--- pl4/2.pl
+++ pl4/2.pl
@@ -1,3 +1,5 @@
+:- use_module(library(lists)).
+
 % flight(origin, destination, company, code, hour, duration)
 
 flight(porto, lisbon, tap, tp1949, 1615, 60).
@@ -15,3 +17,39 @@ flight(porto, frankfurt, lufthansa, lh1177, 1230, 165).
 
 
 % a)
+
+get_all_nodes(ListOfAirports) :- setof(Airport, (Or,Dest,Comp,Code,Hour,Dur)^(flight(Or, Dest, Comp, Code, Hour, Dur), (Or=Airport; Dest=Airport)), ListOfAirports).
+
+% b)
+
+score(Company, Score) :- setof(Destination, (Or, Code, Hour, Dur)^flight(Or, Destination, Company, Code, Hour, Dur), Destinations),
+                         length(Destinations, Score).
+
+most_diversified([Company], Company).
+most_diversified([Company | T], Company) :- most_diversified(T, NextBest),
+                                            score(Company, BestScore),
+                                            score(NextBest, NextBestScore),
+                                            BestScore >= NextBestScore.
+most_diversified([Competitor | T], Company) :- score(Competitor, CompScore),
+                                               score(Company, BestScore),
+                                               BestScore >= CompScore,
+                                               most_diversified(T, Company).
+
+most_diversified(Company) :- setof(Company, (Or,Dest,Code,Hour,Dur)^flight(Or, Dest, Company, Code, Hour, Dur), Companies),
+                             most_diversified(Companies, Company).
+
+% c)
+
+already_visited(Location, [Code | T]) :- flight(_, Location, _, Code, _, _);
+                                         flight(Location, _, _, Code, _, _);
+                                         already_visited(Location, T).
+
+find_flights(Origin, Origin, Acc, Acc).
+find_flights(Origin, Destination, Acc, Flights) :- flight(Origin, Intermediate, _, FirstFlight, _, _),
+                                                   \+ already_visited(Intermediate, Acc),
+                                                   find_flights(Intermediate, Destination, [FirstFlight | Acc], Flights).
+
+
+find_flights(Origin, Destination, Flights) :- find_flights(Origin, Destination, [], Path),
+                                              reverse(Path, Flights).
+
